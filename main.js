0c3550ea525a35fb826830f899acdba7c2e0339a
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-10-08 23:45:25 -0700

    Commit for Git Hub Pages

---
+++ main.js
@@ -0,0 +1,38 @@
+//Get modal element 
+var modal = document.getElementById("simpleModal");
+//Get open modal button
+var modalBtn = document.getElementById("modalBtn");
+//Get Close Btn
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+
+//Listen for open click
+
+modalBtn.addEventListener('click', openModal);
+
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//Listen for outside click
+window.addEventListener('click', clickOutside);
+
+//Function to open modal
+
+function openModal() {
+    modal.style.display = 'block';
+};
+
+//Function to close modal
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+
+//Function to close modal
+
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+}
